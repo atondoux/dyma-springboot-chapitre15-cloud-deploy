66ea44122c02d1b084b5b1c1445729999ac18cca
Aurélien <atondoux@gmail.com>
2024-03-05 23:11:58 +0100

    :bug: fix csp config mixed content

--- src/main/java/com/dyma/tennis/security/SecurityConfiguration.java
+++ src/main/java/com/dyma/tennis/security/SecurityConfiguration.java
@@ -43,7 +43,7 @@ public class SecurityConfiguration {
                         headers
                                 .contentSecurityPolicy(csp ->
                                         csp.policyDirectives("default-src 'self' data:;" +
-                                                "connect-src 'self' http: https:;" +
+                                                "connect-src 'self' http: https: data:;" +
                                                 "style-src 'self' maxcdn.bootstrapcdn.com getbootstrap.com 'unsafe-inline';"))
                                 .frameOptions(frameOptionsConfig -> frameOptionsConfig.deny())
                                 .permissionsPolicy(permissions ->
